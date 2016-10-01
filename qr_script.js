cecde9bc6dcf8da65c0c66cd07f8c7d46312d03c
LiebeLibelle <liebe_libelle@mail.ru>
2016-10-01 16:24:23 +0300

    add table template and scoreboards's data

--- qr_script.js
+++ qr_script.js
@@ -1,33 +1,18 @@
-/* var elem = document.getElementById("scoreboard");
+var scoreboards = [
+  {id: 1, name: "Проспект просвещения", uptime: 134, temp: 23, voltage: 12.543},
+  {id: 2, name: "Светлановская площадь", uptime: 512, temp: 25, voltage: 13.089},
+  {id: 3, name: "Пионерская", uptime: 98, temp: 19, voltage: 12.991}
+];
 
-var elemRow = elem.insertRow(2); */
+var tableTemplate = [
+  {columnName: "ID платы", key: "id"},
+  {columnName: "Название остановки", key: "name"},
+  {columnName: "Время работы", key: "uptime"},
+  {columnName: "Температура", key: "temp"},
+  {columnName: "Напряжение", key: "voltage"}
+];
 
-
-var tableData = {
-  ids:[1, 2, 3],
-  names:["Проспект просвещения", "Светлановская площадь", "Пионерская"],
-  uptimes:[134, 512, 98],
-  temps:[23, 25, 19],
-  voltages:[12.543, 13.089, 12.991]
-};
-
-function drawTable(table) {
-  var body = [];
-  var captions = [];
-  for(caption in table) {
-    captions.push(caption);
-    body.push(table[caption]);
-  }
-  console.log(">:",captions.join("\t\t"));
-  
-  
-  for(var rowIdx = 0; rowIdx < body[0].length; rowIdx++) {
-    var row = [];
-    body.forEach(function(elem) {
-      row.push(elem[rowIdx]);
-    });
-    console.log((rowIdx + 1).toString(), ":", row.join("\t\t"));
-  }
+function drawTable(template, data) {
   
 }
 
@@ -36,10 +21,11 @@ function ready() {
   var scoreboard = document.getElementById("scoreboard");
   console.log(scoreboard);
   var row = scoreboard.insertRow(-1);
+  
   /*for(var i = 0; i < 13;i++) {
     var cell = row.insertCell(i);
     cell.innerHTML = "foo";
   }*/
   
-  drawTable(tableData);
+  drawTable(tableTemplate, scoreboards);
 }
